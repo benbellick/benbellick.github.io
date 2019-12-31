43b0a8d4a8ca7b63e27cc67f4459b3d7d6492267
Ben Bellick <benbellick@gmail.com>
2019-12-31 02:49:38 -0500

    Updates

---
+++ precache-manifest.dd68a1fb11bd5c8a5d27047a91ef80fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5e4a6f233a0847a1e535c0c900275a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82e100d5be5154585fab",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5b8f3ca297eb6fd36aa5",
+    "url": "/static/js/2.ad5eb13b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.ad5eb13b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "82e100d5be5154585fab",
+    "url": "/static/js/main.86a1155c.chunk.js"
+  },
+  {
+    "revision": "c7aea7bf847415a178e3",
+    "url": "/static/js/runtime-main.5a227522.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
